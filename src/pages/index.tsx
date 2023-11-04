fafb6f9f76fdbf513c8e61247f70d1cc013a70ac
Allan Kenneth Rosal <allankennethrosal@gmail.com>
2023-11-04 16:53:54 +0800

    Setup pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
-import Head from 'next/head'
-import styles from '@/styles/Home.module.css'
+import Head from 'next/head';
+import styles from '@/styles/Home.module.css';
+import Dashboard from './dashboard';
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
+        <Dashboard />
       </main>
     </>
   )
