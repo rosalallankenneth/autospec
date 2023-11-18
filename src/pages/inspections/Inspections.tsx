b7284b5fa96a3fcd68e2b53c4438b17ae5001445
Allan Kenneth Rosal <allankennethrosal@gmail.com>
2023-11-19 00:52:31 +0800

    Added next/head to all pages

--- src/pages/inspections/Inspections.tsx
+++ src/pages/inspections/Inspections.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import Head from "next/head";
 import { Box } from "@mui/material";
 import MainLayout from "@/layouts/MainLayout";
 
 const Inspections = () => {
-	return <Box>Inspections</Box>;
+	return (
+		<>
+			<Head>
+				<title>Inspections | Autospec</title>
+				<meta name="description" content="Vehicle Inspection Report System" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<Box>Inspections</Box>
+		</>
+	);
 };
 
 Inspections.getLayout = (page: any) => <MainLayout>{page}</MainLayout>;
--- src/pages/schedules/Schedules.tsx
+++ src/pages/schedules/Schedules.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import Head from "next/head";
 import { Box } from "@mui/material";
 import MainLayout from "@/layouts/MainLayout";
 
 const Schedules = () => {
-	return <Box>Schedules</Box>;
+	return (
+		<>
+			<Head>
+				<title>Schedules | Autospec</title>
+				<meta name="description" content="Vehicle Inspection Report System" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<Box>Schedules</Box>
+		</>
+	);
 };
 
 Schedules.getLayout = (page: any) => <MainLayout>{page}</MainLayout>;
